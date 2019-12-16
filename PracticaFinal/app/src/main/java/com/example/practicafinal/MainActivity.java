c8e6e721cb2eefad32c3e417838347337735658e
alejandrodfs <alejandro97dfs@gmail.com>
2019-12-16 11:41:48 +0100

    NuevoCambio

--- PracticaFinal/app/src/main/java/com/example/practicafinal/MainActivity.java
+++ PracticaFinal/app/src/main/java/com/example/practicafinal/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.practicafinal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ListView milista = (ListView)findViewById(R.id.milista);
+        String[] values = new String[]{"Ironman","Capitan America","Hulk","Thor","Black Widow","Ant man","Spider man"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, values);
+        milista.setAdapter(adapter);
     }
+
 }
