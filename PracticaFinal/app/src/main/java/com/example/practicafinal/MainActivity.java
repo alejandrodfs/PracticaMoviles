280aaf08adc2680a0b172a69a3b1c576a63dcc7c
alejandrodfs <alejandro97dfs@gmail.com>
2019-12-17 17:53:45 +0100

    Vistas
    Vistas y algo de lógica

--- PracticaFinal/app/src/main/java/com/example/practicafinal/MainActivity.java
+++ PracticaFinal/app/src/main/java/com/example/practicafinal/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.practicafinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -16,13 +18,30 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ListView milista = (ListView)findViewById(R.id.milista);
-        String[] values = new String[]{"Ironman","Capitan America","Hulk","Thor","Black Widow","Ant man","Spider man"};
+        final ListView milista = findViewById(R.id.milista);
+        String[] values = new String[]{"Ironman","Capitan America","Hulk","Thor","Black Widow","Ant man","Spider man","Capitan America","Hulk","Thor","Black Widow","Ant man","Spider man"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, values);
         milista.setAdapter(adapter);
+        milista.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+                int item = position;
+                String itemval = (String)milista.getItemAtPosition(position);
+                Toast.makeText(getApplicationContext(), "Position: "+ item+" – Valor: "+itemval, Toast.LENGTH_LONG).show();
+            }
+
+        });
+
+    }
+
+    public void clickBotonCrear( View v ){
+        //Debemos pasar el nivel con el intent
+        Intent intent = new Intent(MainActivity.this, CreateRegister.class);
+        startActivity(intent);
     }
 
 }
