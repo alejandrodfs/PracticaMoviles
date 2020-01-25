dd3e80380b016f3d4ec5b8305f3ee61773d02acc
alejandrodfs <alejandro97dfs@gmail.com>
2020-01-25 10:42:42 +0100

    Menu

--- PracticaFinal/app/src/main/java/com/example/practicafinal/MainActivity.java
+++ PracticaFinal/app/src/main/java/com/example/practicafinal/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.practicafinal;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -25,9 +29,29 @@ public class MainActivity extends AppCompatActivity {
     private String busqueda ;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        final DBHelper dbHelper = new DBHelper(this);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
 
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.listReceta:
+                startActivity(new Intent(this, MainActivity.class));
+                return true;
+            case R.id.createReceta:
+                startActivity(new Intent(this, CreateRegister.class));
+                return true;
+
+            default:
+                return false;
+        }
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
         setTheme(R.style.AppTheme);
         try{
@@ -36,52 +60,46 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
         DBHelper receta = new DBHelper(this);
         SQLiteDatabase db = receta.getWritableDatabase();
         //Pasamos el parametro busqueda, si este es nulo devuelve todas las recetas sino hace la busqueda
         List<Receta> recetas = DBHelper.getRecetas(db);
 
-
-        ArrayAdapter<Receta> adapter;
-
-        final ListView milista = findViewById(R.id.milista);
-
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,recetas);
-        milista.setAdapter(adapter);
+        if(!recetas.isEmpty()){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_receta_list);
 
 
-        milista.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            ArrayAdapter<Receta> adapter;
 
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
-                int item = position;
-                Receta itemval = (Receta)milista.getItemAtPosition(position);
-                String titulo = itemval.getTitulo();
+            final ListView milista = findViewById(R.id.milista);
 
-                Toast.makeText(getApplicationContext(), "Has seleccionado la receta: "+titulo, Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(MainActivity.this, ShowRegister.class);
-                Bundle b = new Bundle();
-                //pasamos el id para asi hacer una consulta en la base de datos y sacar todos los campos
-                b.putString("RecetaID", itemval.getId());
-                intent.putExtras(b);
-                startActivity(intent);
-            }
-
-        });
+            adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,recetas);
+            milista.setAdapter(adapter);
 
 
+            milista.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+                    int item = position;
+                    Receta itemval = (Receta)milista.getItemAtPosition(position);
+                    String titulo = itemval.getTitulo();
+
+                    Toast.makeText(getApplicationContext(), "Has seleccionado la receta: "+titulo, Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(MainActivity.this, ShowRegister.class);
+                    Bundle b = new Bundle();
+                    //pasamos el id para asi hacer una consulta en la base de datos y sacar todos los campos
+                    b.putString("RecetaID", itemval.getId());
+                    intent.putExtras(b);
+                    startActivity(intent);
+                }
+
+            });
+        } else {
+            setContentView(R.layout.activity_main);
+        }
     }
 
-    public void clickBotonCrear( View v ){
-        //Debemos pasar el nivel con el intent
-        Intent intent = new Intent(MainActivity.this, CreateRegister.class);
-        startActivity(intent);
     }
-
-
-
 
 }
---
+++ PracticaFinal/app/src/main/java/com/example/practicafinal/RecetaList.java
@@ -0,0 +1,76 @@
+package com.example.practicafinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class RecetaList extends AppCompatActivity {
+
+    private EditText editTextBusqueda;
+    private SQLiteDatabase db = null;
+    private SQLiteDatabase db2 = null;
+    private String busqueda ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+        setTheme(R.style.AppTheme);
+        try{
+            Thread.sleep(2000);
+        }catch(Exception e){
+
+        }
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receta_list);
+
+
+        DBHelper receta = new DBHelper(this);
+        SQLiteDatabase db = receta.getWritableDatabase();
+        //Pasamos el parametro busqueda, si este es nulo devuelve todas las recetas sino hace la busqueda
+        List<Receta> recetas = DBHelper.getRecetas(db);
+
+
+        ArrayAdapter<Receta> adapter;
+
+        final ListView milista = findViewById(R.id.milista);
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,recetas);
+        milista.setAdapter(adapter);
+
+
+        milista.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+                int item = position;
+                Receta itemval = (Receta)milista.getItemAtPosition(position);
+                String titulo = itemval.getTitulo();
+
+                Toast.makeText(getApplicationContext(), "Has seleccionado la receta: "+titulo, Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(RecetaList.this, ShowRegister.class);
+                Bundle b = new Bundle();
+                //pasamos el id para asi hacer una consulta en la base de datos y sacar todos los campos
+                b.putString("RecetaID", itemval.getId());
+                intent.putExtras(b);
+                startActivity(intent);
+            }
+
+        });
+
+
+    }
+
+
+}
